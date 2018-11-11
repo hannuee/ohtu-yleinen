b750c4cf2fc48c7476dc77f9a177686970cc93fc
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2018-11-11 21:07:13 +0200

    Viikko 2 tehtävä 10

---
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/KirjanpitoRajapinta.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Hannu
+ */
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public interface KirjanpitoRajapinta {
+
+    ArrayList<String> getTapahtumat();
+
+    void lisaaTapahtuma(String tapahtuma);
+    
+}
