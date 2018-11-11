cfd158b41a5561e077dc33430bbacdea495fb56a
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2018-11-11 22:53:47 +0200

    Viikko 2 tehtävä 13

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,12 +1,17 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.*;
 
+@Component
 public class Varasto implements VarastoRajapinta {
     
     private KirjanpitoRajapinta kirjanpito;
     private HashMap<Tuote, Integer> saldot;  
     
+    @Autowired
     public Varasto(KirjanpitoRajapinta kirjanpito) {
         this.kirjanpito = kirjanpito;
         saldot = new HashMap<Tuote, Integer>();
